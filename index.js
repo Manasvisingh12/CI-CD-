965ee23316589117f57bf81612adb73edc0b4f13
Manasvisingh12 <146693822+Manasvisingh12@users.noreply.github.com>
2026-02-12 01:50:48 +0530

    added CI/CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Version 1");
+  res.send("Version 2");
 });
 
 app.get("/health", (req, res) => {
